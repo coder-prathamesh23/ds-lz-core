4c27da1f7daa5e5e4afb99c72e4fdec08f1598fc
coder-prathamesh23 <157107915+coder-prathamesh23@users.noreply.github.com>
2026-03-16 21:48:18 +0530

    Update versions.tf

--- modules/data-science-lz-core/versions.tf
+++ modules/data-science-lz-core/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.7.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.21"
-      configuration_aliases = [ azurerm.hub ]
     }
   }
 }
