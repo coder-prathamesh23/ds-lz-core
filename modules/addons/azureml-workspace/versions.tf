6df04d37e7044a0df8a825f14375ff07bac39c27
coder-prathamesh23 <157107915+coder-prathamesh23@users.noreply.github.com>
2026-03-16 21:50:06 +0530

    Update versions.tf

--- modules/addons/azureml-workspace/versions.tf
+++ modules/addons/azureml-workspace/versions.tf
@@ -1,9 +1,15 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.7.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.21"
+    }
+
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4"
     }
   }
 }
